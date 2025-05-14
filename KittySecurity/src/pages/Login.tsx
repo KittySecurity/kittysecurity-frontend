4c804ba9c7af193d2a80de00a705e452f0279aea
piotrek1kons <piotrek1kons@gmail.com>
2025-05-14 21:34:37 +0200

    feat: arrangement of login page v1

--- KittySecurity/src/pages/Login.tsx
+++ KittySecurity/src/pages/Login.tsx
@@ -2,6 +2,9 @@ import { useNavigate } from "react-router";
 import { Link } from "react-router";
 import Header from "../components/Header"
 import { useState } from "react";
+import "../styles/Login.css"
+import patternBot from "../assets/wzorki2.svg"
+import patternTop from "../assets/wzorki3.svg"
 
 function Login(){
     const navigate = useNavigate();
@@ -14,34 +17,44 @@ function Login(){
     }
 
     return(
-        <div className="login-container">
+        <div className="login-header">
             <Header />
+            <div className="login-container">
+            <div className="pattern-bot">
+                <img src={patternBot} alt="pattern-bot"/>
+            </div>
+            <div className="pattern-top">
+                <img src={patternTop} alt="pattern-top"/>
+            </div>
             <div className="login-form">
                 <h1>SIGN IN</h1>
                 <p>and manage your passwords in one place – safely and conveniently!</p>
                 <form onSubmit={handleSignin}>
-                    <div>
+                    <div className="e-mail">
                         <label>Email:</label>
+                        <br/>
                         <input type="email" 
                         placeholder="example@example.com"
                         value={email}
                         onChange={(e) => setEmail(e.target.value)}/>
                     </div>
-                    <div>
+                    <div className="password">
                         <label>Password:</label>
+                        <br/>
                         <input type="email" 
                         placeholder="super secret password"
                         value={password}
                         onChange={(e) => setPassword(e.target.value)}/>
                     </div>
-                    <Link to="/forgot-password">I FORGOT MY PASSWORD</Link>
+                    <Link className="forgot" to="/forgot-password">I FORGOT MY PASSWORD</Link>
                     <div>
                         <button type="submit">SIGN IN</button>
                     </div>
                 </form>
             </div>
-            <div>
+            <div className="account">
                 <Link to="/register">DON'T HAVE AN ACCOUNT YET? SIGN UP HERE</Link>
+            </div>
             </div>
         </div>
     )

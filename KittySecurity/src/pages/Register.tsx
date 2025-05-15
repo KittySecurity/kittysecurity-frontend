b3b0134ae92a2d942948890d2d8f27926970b01c
Peter <peter.saletra@gmail.com>
2025-05-16 00:42:34 +0200

    feat: added custom strength password indicator

--- KittySecurity/src/pages/Register.tsx
+++ KittySecurity/src/pages/Register.tsx
@@ -12,6 +12,7 @@ function Register(){
     const [login, setLogin] = useState<string>()
     const [email, setEmail] = useState<string>();
     const [password, setPassword] = useState<string>();
+    const [strength, setStrength] = useState<number>(0.0);
     const [passwordConfiramtion, setPasswordConfirmation] = useState<string>();
 
 
@@ -23,6 +24,38 @@ function Register(){
             navigate("/login");
             setError(null);
         }
+    }
+
+    function evaluatePassword(password: string): number {
+        let score = 0;
+
+        if (!password) return 0;
+
+        if (password.length > 10) score += 1;
+        if (/[a-z]/.test(password)) score += 1;
+        if (/[A-Z]/.test(password)) score += 1;
+        if (/\d/.test(password)) score += 1;
+        if (/[^A-Za-z0-9]/.test(password)) score += 1;
+        if (/[-*%!@#.,?/_+<>;:]/.test(password)) score += 1;
+
+        if (!(login && password.includes(login))) {
+            score += 1;
+            setError(null);
+        }else{
+            setError("Password should not contain your username");
+        }
+
+        if (email) {
+            const [local, domain] = email.split("@");
+            if (!(local && password.includes(local)) && !(domain && password.includes(domain))) {
+                score += 1;
+                setError(null);
+            }else{
+                setError("Password should not contain parts of your email");
+            }
+        }
+
+        return score / 8;
     }
 
     return(
@@ -58,7 +91,17 @@ function Register(){
                             <input type="password" 
                             placeholder="super secret password"
                             value={password}
-                            onChange={(e) => setPassword(e.target.value)}/>
+                            onChange={(e) => {setPassword(e.target.value); setStrength(evaluatePassword(e.target.value));}}/>
+                        </div>
+                        <div className="strong-password">
+                            <label>Password strength:</label>
+                            <progress className="strong-password-bar" value={strength}/>
+                            <div className="strong-password-text">
+                                {strength < 0.3 && <span>Very weak</span>}
+                                {strength > 0.3 && strength <= 0.5 && <span>Weak</span>}
+                                {strength > 0.5 && strength <= 0.8 && <span>Medium</span>}
+                                {strength > 0.8 && strength <= 1 && <span>Strong</span>}
+                            </div>
                         </div>
                         <div className="confirm-password">
                             <label>Confirm Password:</label>
@@ -67,6 +110,7 @@ function Register(){
                             value={passwordConfiramtion}
                             onChange={(e) => setPasswordConfirmation(e.target.value)}/>
                         </div>
+                        {error && <div className="error">{error}</div>}
                         <div>
                             <button type="submit">SIGN UP</button>
                         </div>

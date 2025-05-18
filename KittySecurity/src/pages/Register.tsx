325d8ae674798704107cb0d2b345b87d48fdd121
piotrek1kons <piotrek1kons@gmail.com>
2025-05-18 20:38:59 +0200

    fix: fixed login and register page responsiveness

--- KittySecurity/src/pages/Register.tsx
+++ KittySecurity/src/pages/Register.tsx
@@ -3,8 +3,8 @@ import { Link } from "react-router";
 import Header from "../components/Header"
 import { useState, useEffect } from "react";
 import "../styles/Register.css"
-import patternBot from "../assets/wzorki2.svg"
-import patternTop from "../assets/wzorki3.svg"
+import patternBotReg from "../assets/wzorki2.svg"
+import patternTopReg from "../assets/wzorki3.svg"
 
 function Register(){
     const navigate = useNavigate();
@@ -72,10 +72,10 @@ function Register(){
             <Header />
             <div className="register-container">
                 <div className="pattern-bot">
-                    <img src={patternBot} alt="pattern-bot"/>
+                    <img src={patternBotReg} alt="pattern-bot"/>
                 </div>
                 <div className="pattern-top">
-                    <img src={patternTop} alt="pattern-top"/>
+                    <img src={patternTopReg} alt="pattern-top"/>
                 </div>
                 <div className="register-form">
                     <h1>Sign up</h1>
@@ -126,7 +126,7 @@ function Register(){
                             value={passwordConfiramtion}
                             onChange={(e) => setPasswordConfirmation(e.target.value)}/>
                         </div>
-                        <Link to="/strong-password">HOW TO CREATE STRONG PASSWORD? CHECK IT OUT HERE</Link>
+                        <Link className="strong-password-link" to="/strong-password">HOW TO CREATE STRONG PASSWORD? CHECK IT OUT HERE</Link>
                         <div>
                             <button type="submit">SIGN UP</button>
                         </div>

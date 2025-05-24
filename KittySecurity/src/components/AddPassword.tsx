e480d678c1569858e02817e557e736bcd1cb2357
Asasinxd <peter.saletra@gmail.com>
2025-05-24 16:44:09 +0200

    feat: Implemented UI design into AddPassowrd

--- KittySecurity/src/components/AddPassword.tsx
+++ KittySecurity/src/components/AddPassword.tsx
@@ -1,31 +1,45 @@
+import "../styles/AddPassowrd.css"
+import Generate from "../assets/generate.svg"
+import Settings from "../assets/settings.svg"
+
 const AddPassword = ({ onClose }: { onClose: () => void }) => {
 
     return (
         <div className="modal">
           <div className="modal-content">
-            <h2>ADD NEW DEVICE</h2>
-            <label>NAME</label>
-            <input
-                type="text"
-                placeholder='Platform Name'
-            />
+            <div className="modal-input">
+              <h2>ADD NEW DEVICE</h2>
+              <label>NAME</label>
+              <input
+                  type="text"
+                  placeholder='Platform Name'
+              />
+            </div>
+            <div className="modal-input">
             <label>E-MAIL/USERNAME</label>
             <input
                 type="text"
                 placeholder='email@example.com'
             />
-
-            <label>URL</label>
-            <input
-                type="text"
-                placeholder='https://example.com'
-            />
-            
-            <label>PASSWORD</label>
-            <input
-                type="text"
-                placeholder='Super Secret Password'
-                />
+            </div>
+            <div className="modal-input">
+              <label>URL</label>
+              <input
+                  type="text"
+                  placeholder='https://example.com'
+              />
+            </div>
+            <div className="modal-input">
+              <label>PASSWORD</label>
+              <div className="password-input">
+              <input
+                  type="text"
+                  placeholder='Super Secret Password'
+                  />
+              <img src={Generate} alt="generate-img" />
+              <img src={Settings} alt="settings-img" />
+              </div>
+            </div>
             <div className="modal-actions">
               <button >SAVE</button>
               <button onClick={onClose}>CANCEL</button>
